893a84b38a4c3e7d62f161f3dc4b99e3f2371537
wcfx <wcfx.dev@gmail.com>
2020-12-04 01:22:05 -0300

    layout login finalizado. Criando funcao de localizacao

--- src/views/Home/index.js
+++ src/views/Home/index.js
@@ -1,13 +1,50 @@
-import React from 'react';
-import { View, Text } from 'react-native';
+import React, { useState } from 'react';
+import { useNavigation } from '@react-navigation/native';
+import {
+  Container,
+  Scroller,
+  HeaderArea,
+  HeaderTitle,
+  SearchButton,
+  LocationArea,
+  LocationInput,
+  LocationFinder,
+} from './styles';
 
-// import { Container } from './styles';
+import SearchIcon from '../../assets/search.svg';
+import MyLocationIcon from '../../assets/my_location.svg';
 
 const Home = () => {
+  const { navigate } = useNavigation();
+  const [locationText, setLocationText] = useState('');
+
+  function handleNavigateToSearch() {
+    navigate('Search');
+  }
+
   return (
-    <View>
-      <Text>Olá</Text>
-    </View>
+    <Container>
+      <Scroller>
+        <HeaderArea>
+          <HeaderTitle>Encontre o seu {'\n'}barbeiro favorito.</HeaderTitle>
+          <SearchButton>
+            <SearchIcon width="26" height="26" fill="#FFF" />
+          </SearchButton>
+        </HeaderArea>
+        <LocationArea>
+          <LocationInput
+            placeholder="Me informe onde você está"
+            placeholderTextColor="#ddd"
+            value={locationText}
+            onChangeText={setLocationText}
+            onPress={handleNavigateToSearch}
+          />
+          <LocationFinder>
+            <MyLocationIcon width="24" height="24" fill="#FFF" />
+          </LocationFinder>
+        </LocationArea>
+      </Scroller>
+    </Container>
   );
 };
 
